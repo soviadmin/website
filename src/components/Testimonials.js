0229fdf1366c162c90614047162d70f8ec8e94fe
Katie Le <khanhlevany@gmail.com>
2020-11-07 12:44:36 +0700

    change section colors

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
-
 export default function Testimonials() {
     // const imgContainer = useImage();
     return (
         <div>
-            <div className="feature-eq choose-us">
-                <h3>See why people choose us</h3>
+            <div className="testimonials why-choose-us">
+                <h2>See why people choose us</h2>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-4 col-xs-12 pr-5 pl-5">
-                            <h5>Fast</h5>
+                            <h5>Fast execution</h5>
                             <p>Find aute irure dolor in reprehend in voluptate velit esse cillum dolore eu</p>
                         </div>
                         <div className="col-lg-4 col-xs-12 pr-5 pl-5">
-                            <h5>Reliable</h5>
+                            <h5>Reliability</h5>
                             <p>Find aute irure dolor in reprehend in voluptate velit esse cillum dolore eu</p>
                         </div>
                         <div className="col-lg-4 col-xs-12 pr-5 pl-5">
-                            <h5>Customer service</h5>
+                            <h5>Quality assurance</h5>
                             <p>Find aute irure dolor in reprehend in voluptate velit esse cillum dolore eu</p>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="feature-eq">
-                <h3>Our Partners</h3>
+
+            <div className="testimonials partners">
+                <h2>Our Partners</h2>
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-2 col-xs-12">
